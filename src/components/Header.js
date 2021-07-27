d1965161e6188c354f6113295e61e46c88946429
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-07-27 04:32:05 -0500

    added-border-radius

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     <div className='header1'>
         <Logo />
      <h1 className='title'>El ChinChorro De Frida</h1> 
+     <h2 className='title1'>De martes  a Sabados </h2>
     </div>
   )
 }
